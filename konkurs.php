f69ecda75eeb3115217a3ecbc6d4973759c455b0
00chestnut <maksymilian@witoszek.com>
2025-05-22 22:27:58 +0200

    Dawdawd

---
+++ konkurs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <link rel="stylesheet" href="css/konkurs.css">
+    <title>WOLONTARIAT SZKOLNY</title>
+</head>
+<body>
+    <header><h1>KONKURS - WOLONTARIAT SZKOLNY</h1></header>
+    <section><h3>Konkursowe nagrody
+        <table>
+            <td></td>
+        </table>
+    </h3></section>
+    <aside></aside>
+    <footer></footer>
+</body>
+</html>
